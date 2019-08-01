c6ec462322de96b84eefa47425f606fb616db832
isyouxiu <3023632683@qq.com>
2019-08-01 16:38:50 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 在控制台的提示信息  productionTip = false生成换环境提示为false
 Vue.config.productionTip = false
 
+// 跟实力 mount挂载到某个位置
 new Vue({
   render: h => h(App)
 }).$mount('#app')
