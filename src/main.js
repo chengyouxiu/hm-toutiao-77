bf2b80d93a5b51a348c0121c99ba228fcbd8629f
isyouxiu <3023632683@qq.com>
2019-08-01 19:38:41 +0800

    登陆模块路由及组件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router'
 // 在控制台的提示信息  productionTip = false生成换环境提示为false
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 // 跟实力 mount挂载到某个位置
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
