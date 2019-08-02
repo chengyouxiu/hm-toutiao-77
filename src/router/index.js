908d3e3a88455dccbe862ebede03da79b44f1db1
isyouxiu <3023632683@qq.com>
2019-08-02 21:24:36 +0800

    首页结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,22 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login'
+import Home from '@/views/home'
+import welcome from '@/views/welcome'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/login', name: 'login', component: Login }
+    { path: '/login', name: 'login', component: Login },
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', name: 'welcome', component: welcome }
+      ]
+    }
   ]
 })
 export default router
---
+++ src/views/home/index.vue
@@ -0,0 +1,133 @@
+<template>
+  <el-container class="wrapper">
+      <!-- 默认false就是导航栏默认的宽度为200px，点击按钮切换为64px -->
+    <el-aside :width="isCollapse?'64px':'200px'">
+        <!-- 小logo图片 -->
+      <div class="logo" :class="{miniLogo:isCollapse}"></div>
+      <!-- 导航菜单 -->
+      <el-menu
+          :collapse = isCollapse
+          :collapse-transition = "false"
+          default-active="1"
+          background-color="#002033"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-menu-item index="1">
+            <i class="el-icon-s-home"></i>
+            <span slot="title">首页</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <i class="el-icon-document"></i>
+            <span slot="title">内容管理</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-picture"></i>
+            <span slot="title">素材管理</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-s-promotion"></i>
+            <span slot="title">发布文章</span>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <i class="el-icon-chat-dot-round"></i>
+            <span slot="title">评论管理</span>
+          </el-menu-item>
+          <el-menu-item index="6">
+            <i class="el-icon-present"></i>
+            <span slot="title">粉丝管理</span>
+          </el-menu-item>
+          <el-menu-item index="7">
+            <i class="el-icon-setting"></i>
+            <span slot="title">个人设置</span>
+          </el-menu-item>
+        </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <span class="el-icon-s-fold" @click="toggleMenu()"></span>
+        <span class="text">江苏传智播客科技教育有限公司</span>
+        <el-dropdown class="my-dropdown">
+          <span class="el-dropdown-link">
+            <img src="../../assets/images/avatar.jpg" alt />
+            用户名称
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人设置</el-dropdown-item>
+            <el-dropdown-item>退出登陆</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+      <el-main>
+          <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data () {
+    // 按钮切换时导航栏所显示的大小 iscollapse为false默认宽度为200px
+    return {
+      isCollapse: false
+    }
+  },
+  methods: {
+    toggleMenu () {
+      this.isCollapse = !this.isCollapse
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+//标签的名称就是解析后标签的类名
+.wrapper {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  .el-aside {
+    background: #002033;
+  }
+  .el-header {
+    border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .el-icon-s-fold {
+      font-size: 24px;
+      vertical-align: middle;
+    }
+    .text {
+      vertical-align: middle;
+      padding-left: 10px;
+    }
+  }
+}
+.my-dropdown {
+  float: right;
+  img {
+    width: 30px;
+    height: 30px;
+    vertical-align: middle;
+  }
+  .el-dropdown-link {
+    font-weight: 700;
+  }
+}
+.logo {
+  width: 100%;
+  height: 60px;
+  background: #002244 url(../../assets/images/logo_admin.png) no-repeat center /
+    140px auto;
+}
+.miniLogo {
+  background-image: url(../../assets/images/logo_admin_01.png);
+  background-size: 36px auto;
+}
+.el-menu {
+    border-right: none;
+}
+</style>
