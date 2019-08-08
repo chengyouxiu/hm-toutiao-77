ce7da785ee285f39a2741b8296bda260518cbac8
isyouxiu <3023632683@qq.com>
2019-08-08 14:40:52 +0800

    内容管理操作

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,18 @@
 import axios from 'axios'
 import store from '@/store'
 import router from '@/router'
+import JSONBig from 'json-bigint'
 // 配置axios
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+axios.defaults.transformResponse = [
+  (data) => {
+    try {
+      return JSONBig.parse(data)
+    } catch (e) {
+      return data
+    }
+  }
+]
 // axios.defaults.headers = {
 //   Authorization: `Bearer ${store.getUser().token}`
 // }
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -15,66 +15,167 @@
             <el-radio :label="3">审核失败</el-radio>
           </el-radio-group>
         </el-form-item>
-       <el-form-item label="频道：">
-          <el-select v-model="reqParams.channel_id" placeholder="请选择">
+        <el-form-item label="频道：">
+          <el-select clearable v-model="reqParams.channel_id" placeholder="请选择">
             <el-option
               v-for="item in channelOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
             ></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="日期：">
-           <el-date-picker
+          <el-date-picker
             v-model="dateArr"
             type="daterange"
             range-separator="至"
             start-placeholder="开始日期"
             end-placeholder="结束日期"
+            @change="changeDate"
+            value-format="yyyy-MM-dd"
           ></el-date-picker>
         </el-form-item>
-         <el-form-item>
-          <el-button type="primary">筛选</el-button>
+        <el-form-item>
+          <el-button type="primary" @click="search()">筛选</el-button>
         </el-form-item>
       </el-form>
     </el-card>
     <!-- 筛选结果 -->
     <el-card>
-      <div slot="header">
-      根据筛选条件共查询到0条数据
-    </div>
-    <!-- 表格组件 -->
-     <el-table :data='articles'>
-     <el-table-column label="封面" prop="img"></el-table-column>
-     </el-table>
-     <!-- 分页组件 -->
-     <div style="text-align:center;margin-top:20px">
-       <el-pagination
-       background
-       layout="prev, pager, next"
-       :total="1000">
-</el-pagination>
-     </div>
+      <div slot="header">根据筛选条件共查询到 {{ total }} 条数据</div>
+      <!-- 表格组件 -->
+      <el-table :data="articles">
+        <el-table-column label="封面" prop="img">
+          <template slot-scope="scope">
+            <el-image :src="scope.row.cover.images[0]" fit="cover" style="width:120px;height:80px">
+              <div slot="error">
+                <img src="../../assets/images/error.gif" style="width:120px;height:80px" alt />
+              </div>
+            </el-image>
+          </template>
+        </el-table-column>
+        <el-table-column label="标题" prop="title"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status === 0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status === 1">待审核</el-tag>
+            <el-tag v-if="scope.row.status === 2" type="success">审核通过</el-tag>
+            <el-tag v-if="scope.row.status === 3" type="warning">审核失败</el-tag>
+            <el-tag v-if="scope.row.status === 4" type="danger">删除</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="发布时间" prop="pubdate"></el-table-column>
+        <el-table-column label="操作" width="120px">
+          <template slot-scope="scope">
+            <el-button plain type="primary" @click="edit(scope.row.id)" icon="el-icon-edit" circle></el-button>
+            <el-button plain type="danger" @click="del(scope.row.id)" icon="el-icon-delete" circle></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页组件 -->
+      <div style="text-align:center;margin-top:20px">
+        <el-pagination
+          background
+          layout="prev, pager, next"
+          :total="total"
+          :page-size="reqParams.per_page"
+          :current-page="reqParams.page"
+          @current-change="changePager"
+        ></el-pagination>
+      </div>
     </el-card>
   </div>
 </template>
 
 <script>
-
+// import { async } from 'q'
 export default {
   data () {
     return {
       // 提交给后台的数reqParams
       reqParams: {
         status: null,
-        channel_id: null
+        channel_id: null,
+        begin_pubdate: null,
+        end_pubdate: null,
+        page: 1,
+        per_page: 20
       },
       // 频道下拉选项数据不需要传给后台所以声明在reqPatams外面
-      channelOptions: [{ value: 1, label: 'js' }],
+      channelOptions: [],
       // 日期数据
       dateArr: [],
-      articles: []
+      // 文章列表
+      articles: [],
+      // 总条数
+      total: 0
+    }
+  },
+  watch: {
+    'reqParams.channel_id': function (newVal, oldVal) {
+      if (newVal === '') {
+        this.reqParams.channel_id = null
+      }
+    }
+  },
+  // 获取频道下拉选项的数据 created()实例创建完成之后请求数据
+  created () {
+    // 获取频道下拉选项的数据
+    this.getChannelOptions()
+    // 获取文章列表数据
+    this.getArticles()
+  },
+  methods: {
+    // 编辑操作
+    edit (id) {
+      this.$router.push('/publish?id=' + id)
+    },
+    // 删除操作
+    del (id) {
+      this.$confirm('亲，此操作将永久删除该文章, 是否继续?', '温馨提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        await this.$http.delete(`articles/${id}`)
+        this.$message.success('删除文章成功')
+        this.getArticles()
+      }).catch(() => {})
+    },
+    // 日期选择后的事件
+    changeDate (dateArr) {
+      // 严谨一些，清空数据考虑在内
+      if (dateArr) {
+        this.reqParams.begin_pubdate = dateArr[0]
+        this.reqParams.end_pubdate = dateArr[1]
+      } else {
+        this.reqParams.begin_pubdate = null
+        this.reqParams.end_pubdate = null
+      }
+    },
+    // 筛选函数 筛选函数是双向绑定拿对应的数据发送请求即可
+    search () {
+      this.reqParams.page = 1
+      this.getArticles()
+    },
+    // 改变分页事件
+    changePager (newPage) {
+      this.reqParams.page = newPage
+      this.getArticles()
+    },
+    async getChannelOptions () {
+      const {
+        data: { data }
+      } = await this.$http.get('channels')
+      this.channelOptions = data.channels
+    },
+    async getArticles () {
+      const {
+        data: { data }
+      } = await this.$http.get('articles', { params: this.reqParams })
+      this.articles = data.results
+      this.total = data.total_count
     }
   }
 }
